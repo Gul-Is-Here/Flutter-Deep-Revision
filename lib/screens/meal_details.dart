353ded2583f537f72bd19f7ab0314dcc81fb8ef8
Gul-Is-Here <98545209+Gul-Is-Here@users.noreply.github.com>
2024-02-21 11:22:15 +0500

    'Flutter Animation basic Complete'

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -34,23 +34,31 @@ class MealDetailsScreen extends ConsumerWidget {
               );
             },
             icon: AnimatedSwitcher(
-              duration: Duration(microseconds: 300),
-              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+              duration: const Duration(microseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
-                    turns: Tween(begin: 0.5, end: 1.0).animate(animation));
+                  turns: Tween(begin: .5, end: 3.0).animate(animation),
+                  child: child,
+                );
               },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           )
         ]),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 14),
               Text(
